b5d64fb49370ae149ad4c37bd3e22b3b2f614718
kashifwebgit5 <gmdcf797@gmail.com>
2024-10-29 18:42:29 +0500

    private route and changing files Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -25,14 +25,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 
 // Crud Perform
-Route::get("user",[UserController::class,"getUser"]);
-Route::get("single/{id}",[UserController::class,"singleUser"]);
-Route::post("add-user",[UserController::class,"createUser"]);
-Route::patch("edit-user/{id}", [UserController::class, "userUpdate"]);
-Route::delete("delete/{id}", [UserController::class, "DeleteUser"]);
-Route::delete("multiple-delete", [UserController::class, "MultipleDelete"]);
-Route::get("search/{name}", [UserController::class, "searchApi"]);
+
 
 // Auth Routing
 Route::post("sigin",[UserAuthController::class,"sigin"]);
 Route::post("login",[UserAuthController::class,"login"]);
+
+
+// Auth 
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('user', [UserController::class, 'getUser']);
+    Route::get('single/{id}', [UserController::class, 'singleUser']);
+    Route::post('add-user', [UserController::class, 'createUser']);
+    Route::patch('edit-user/{id}', [UserController::class, 'userUpdate']);
+    Route::delete('delete/{id}', [UserController::class, 'DeleteUser']);
+    Route::delete('multiple-delete', [UserController::class, 'MultipleDelete']);
+    Route::get('search/{name}', [UserController::class, 'searchApi']);
+});
